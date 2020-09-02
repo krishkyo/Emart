5eb15aa86f1fd9eff32f5960118fb931d920481b
Josephine <69450628+Josephinestella16@users.noreply.github.com>
2020-09-02 16:08:58 +0530

    2nd

--- src/main/java/com/example/demo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends CrudRepository<UserEInfo, String>{
-
-}
